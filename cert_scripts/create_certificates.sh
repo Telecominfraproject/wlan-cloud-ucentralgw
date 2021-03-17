3230cf3795417acb0efdd585e7d071924e975268
stephb9959 <stephane.bourque@gmail.com>
2021-03-16 21:50:49 -0700

    New certificate creation scripts

--- cert_scripts/create_certificates.sh
+++ cert_scripts/create_certificates.sh
@@ -8,7 +8,7 @@ openssl genrsa -out server-key.pem 2048
 openssl req -new -key server-key.pem -subj "$subject" -out server.csr
 openssl x509 -req -days $cert_life -in server.csr -signkey server-key.pem -out server-cert.pem
 
-for i in `eval echo {0..$howmany}`
+for i in `eval echo {1..$howmany}`
 do
   openssl x509 -signkey server-key.pem -in server.csr -req -days $cert_life -out dev-$i-cert.pem
 done
