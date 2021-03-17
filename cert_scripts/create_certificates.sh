89ad2a7ffadc804aada0a5c8b84f0088c13744d3
stephb9959 <stephane.bourque@gmail.com>
2021-03-16 21:36:07 -0700

    New certificate creation scripts

---
+++ cert_scripts/create_certificates.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+howmany=10
+cert_life=365
+subject="/C=CA/ST=British Columbia/L=Vancouver/O=Arilia Wireless/OU=Engineering/CN=ucentral/emailAddress=support@example.com"
+
+openssl genrsa -out server-key.pem 2048
+openssl req -new -key server-key.pem -subj "$subject" -out server.csr
+openssl x509 -req -days $cert_life -in server.csr -signkey server-key.pem -out server-cert.pem
+
+for i in `eval echo {0..$howmany}`
+do
+  openssl x509 -signkey server-key.pem -in server.csr -req -days $cert_life -out dev-$i-cert.pem
+done
