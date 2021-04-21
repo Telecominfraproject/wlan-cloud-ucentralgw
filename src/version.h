1bc81442e4721ddc8d047b844f2b6ecb18a9b2c0
stephb9959 <stephane.bourque@gmail.com>
2021-04-20 20:22:06 -0700

    Adding a --version command line option

---
+++ src/version.h
@@ -0,0 +1,10 @@
+//
+// Created by stephane bourque on 2021-04-20.
+//
+
+#ifndef UCENTRALGW_VERSION_H
+#define UCENTRALGW_VERSION_H
+
+#define __APP_VERSION__	"0.3.1"
+
+#endif // UCENTRALGW_VERSION_H
