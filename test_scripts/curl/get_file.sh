37322af50c5a50432c72b1e081227cd38d9c2305
stephb9959 <stephane.bourque@gmail.com>
2021-05-01 21:40:07 -0700

    Removing olld files

--- test_scripts/curl/get_file.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-webtoken=`./login.sh | jq -r '.access_token'`
-
-curl -X GET --insecure \
-      -H "accept: application/octet-stream" \
-      -H "Authorization: Bearer $webtoken" \
-      --output thefile.bin \
-      "https://localhost:16001/api/v1/file/e8c8ac1a-9e65-11eb-bcee-acde48001122"
--- test_scripts/curl/list_defaultconfigs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-webtoken=`./login.sh | jq -r '.access_token'`
-
-curl -X GET "https://localhost:16001/api/v1/default_configurations" \
-  -H  "accept: application/json" \
-  -H "Authorization: Bearer $webtoken" \
-  --insecure
-
