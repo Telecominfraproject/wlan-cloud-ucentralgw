37b11caac035c4c9e52359ceab738a20933900c4
stephb9959 <stephane.bourque@gmail.com>
2021-05-17 21:40:47 -0700

    Added several new commands.

--- src/RESTAPI_file.cpp
+++ src/RESTAPI_file.cpp
@@ -31,14 +31,18 @@ void RESTAPI_file::handleRequest(Poco::Net::HTTPServerRequest& Request, Poco::Ne
             //does the file exist
             Poco::File  DownloadFile(uCentral::uFileUploader::Path() + "/" + UUID);
 
+			std::cout << "File: " << DownloadFile.path() << std::endl;
+
             if(!uCentral::Storage::GetAttachedFile(UUID,DownloadFile.path()))
             {
                 NotFound(Response);
                 return;
             }
 
+			std::cout << "Preparing to send file" << std::endl;
+
             Response.set("Content-Type","application/octet-stream");
-            Response.set("Content-Disposition", "attachment; filename=" + UUID);
+            Response.set("Content-Disposition", "attachment; filename=" + UUID );
             Response.set("Content-Transfer-Encoding","binary");
             Response.set("Accept-Ranges", "bytes");
             Response.set("Cache-Control", "private");
@@ -46,8 +50,9 @@ void RESTAPI_file::handleRequest(Poco::Net::HTTPServerRequest& Request, Poco::Ne
             Response.set("Expires", "Mon, 26 Jul 2027 05:00:00 GMT");
             Response.set("Content-Length", std::to_string(DownloadFile.getSize()));
             Response.sendFile(DownloadFile.path(),"application/octet-stream");
-
+			std::cout << "File sent..." << std::endl;
 			DownloadFile.remove();
+			std::cout << "File removed..." << std::endl;
 
             return;
 
