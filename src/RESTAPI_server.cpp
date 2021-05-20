555b5f90fed162daee40c4f4879d06ea3d80c92a
stephb9959 <stephane.bourque@gmail.com>
2021-05-20 09:11:36 -0700

    Added several new commands.

--- src/RESTAPI_server.cpp
+++ src/RESTAPI_server.cpp
@@ -61,10 +61,10 @@ namespace uCentral::RESTAPI {
             Params->setMaxThreads(50);
             Params->setMaxQueued(200);
 			Params->setKeepAlive(true);
-			uint64_t T = 45000;
-			Params->setKeepAliveTimeout(T);
-			Params->setMaxKeepAliveRequests(200);
-			Params->setTimeout(T + 10000);
+//			uint64_t T = 45000;
+//			Params->setKeepAliveTimeout(T);
+//			Params->setMaxKeepAliveRequests(200);
+//			Params->setTimeout();
 
             auto NewServer = std::make_unique<Poco::Net::HTTPServer>(new RequestHandlerFactory, Pool_, Sock, Params);
             NewServer->start();
