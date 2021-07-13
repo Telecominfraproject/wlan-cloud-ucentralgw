5f7b13a401be34de0a22586d0a5ff7dd147796b9
oblom0v <johann.hoffmann@mailbox.org>
2021-07-13 16:55:31 +0200

    Remove duplicate ucentralgw-ui entry and adapt Docker Compose CI job to microservice structure
    Adapt new config file changes
    
    Fix script name
    
    Add working-directory
    
    Run docker-compose exec in the background
    
    Test Github actions working-directory behaviour
    
    Revert previous test
    
    Change shebang path
    
    Add some debug information to workflow
    
    Disable pseudo-tty allocation
    
    Fix variable name

--- docker-compose/add-ca-cert.sh
+++ docker-compose/add-ca-cert.sh
@@ -4,7 +4,7 @@ set -e
 SERVICES="ucentralgw.wlan.local ucentralsec.wlan.local"
 
 for i in $SERVICES; do
-    docker-compose exec $i apk add ca-certificates
+    docker-compose exec -T $i apk add ca-certificates
     docker cp certs/restapi-ca.pem ucentral_$i\_1:/usr/local/share/ca-certificates/
-    docker-compose exec $i update-ca-certificates
+    docker-compose exec -T $i update-ca-certificates
 done
