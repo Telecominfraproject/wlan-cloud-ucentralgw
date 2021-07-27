39d05a4cceba32bec8f0772f5d2f6e558a7f5c4c
oblom0v <johann.hoffmann@mailbox.org>
2021-07-27 11:12:21 +0200

    Change to colon separator

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$1" = '/ucentral/ucentralgw' -a "$(id -u)" = '0' ]; then
-    chown -R "$UCENTRALGW_USER". "$UCENTRALGW_ROOT" "$UCENTRALGW_CONFIG"
+    chown -R "$UCENTRALGW_USER": "$UCENTRALGW_ROOT" "$UCENTRALGW_CONFIG"
     exec su-exec "$UCENTRALGW_USER" "$@"
 fi
 
