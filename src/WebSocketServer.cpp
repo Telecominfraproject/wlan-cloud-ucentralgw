319e8918b19decca981b3a18f821a4fec5e6ecd5
stephb9959 <stephane.bourque@gmail.com>
2021-09-01 19:24:15 -0700

    Fixed disconne ctions.

--- src/WebSocketServer.cpp
+++ src/WebSocketServer.cpp
@@ -81,6 +81,7 @@ namespace OpenWifi {
 	}
 
 	void WSConnection::CompleteStartup() {
+		std::lock_guard Guard(Mutex_);
 		try {
 			auto SS = dynamic_cast<Poco::Net::SecureStreamSocketImpl *>(Socket_.impl());
 
@@ -160,7 +161,6 @@ namespace OpenWifi {
     }
 
     WSConnection::~WSConnection() {
-		std::lock_guard Guard(Mutex_);
         DeviceRegistry()->UnRegister(SerialNumber_,this);
         if(Registered_ && WS_)
         {
@@ -583,18 +583,24 @@ namespace OpenWifi {
     }
 
     void WSConnection::OnSocketShutdown(const Poco::AutoPtr<Poco::Net::ShutdownNotification>& pNf) {
+		std::lock_guard Guard(Mutex_);
+
         Logger_.information(Poco::format("SOCKET-SHUTDOWN(%s): Closing.",CId_));
 		std::cout << "Socket shutdown for " << SerialNumber_ << std::endl;
+
         delete this;
     }
 
     void WSConnection::OnSocketError(const Poco::AutoPtr<Poco::Net::ErrorNotification>& pNf) {
+		std::lock_guard Guard(Mutex_);
+
         Logger_.information(Poco::format("SOCKET-ERROR(%s): Closing.",CId_));
         std::cout << "Socket error for " << SerialNumber_ << std::endl;
         delete this;
     }
 
     void WSConnection::OnSocketReadable(const Poco::AutoPtr<Poco::Net::ReadableNotification>& pNf) {
+		std::lock_guard Guard(Mutex_);
         try
         {
             ProcessIncomingFrame();
@@ -633,7 +639,7 @@ namespace OpenWifi {
 			// std::cout << "ID:" << CId_ << " Size=" << IncomingSize << " Flags=" << flags << " Op=" << Op << std::endl;
 
             if (IncomingSize == 0 && flags == 0 && Op == 0) {
-                Logger_.information(Poco::format("DISCONNECT(%s)", CId_));
+                Logger_.information(Poco::format("DISCONNECT(%s): device has disconnected.", CId_));
                 MustDisconnect = true;
             } else {
                 switch (Op) {
@@ -763,14 +769,19 @@ namespace OpenWifi {
             MustDisconnect = true ;
         }
 
-        if(!MustDisconnect || Errors_<10)
+        if(!MustDisconnect && Errors_<10)
             return;
+
+		if(Errors_>10) {
+			Logger_.information(Poco::format("DISCONNECTING(%s): Too many errors",CId_));
+		}
 
         delete this;
     }
 
 	bool WSConnection::Send(const std::string &Payload) {
 		std::lock_guard Guard(Mutex_);
+
 		auto BytesSent = WS_->sendFrame(Payload.c_str(),(int)Payload.size());
 		if(Conn_)
 			Conn_->TX += BytesSent;
