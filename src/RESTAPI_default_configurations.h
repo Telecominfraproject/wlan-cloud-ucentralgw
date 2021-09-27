f73389af3c8bc3bd3ac4d38a056f0d04d0dea95f
stephb9959 <stephane.bourque@gmail.com>
2021-09-27 22:15:37 +0000

    resolve conflicts

--- src/RESTAPI_default_configurations.h
+++ src/RESTAPI_default_configurations.h
@@ -14,14 +14,17 @@
 namespace OpenWifi {
 class RESTAPI_default_configurations : public RESTAPIHandler {
   public:
-	RESTAPI_default_configurations(const RESTAPIHandler::BindingMap &bindings, Poco::Logger &L, bool Internal)
+	RESTAPI_default_configurations(const RESTAPIHandler::BindingMap &bindings, Poco::Logger &L, RESTAPI_GenericServer & Server, bool Internal)
 		: RESTAPIHandler(bindings, L,
 						 std::vector<std::string>{Poco::Net::HTTPRequest::HTTP_GET,
 												  Poco::Net::HTTPRequest::HTTP_OPTIONS},
+						 Server,
 						 Internal){};
-	void handleRequest(Poco::Net::HTTPServerRequest &request,
-					   Poco::Net::HTTPServerResponse &response) override;
 	static const std::list<const char *> PathName() { return std::list<const char *>{"/api/v1/default_configurations"};}
+	void DoGet() final;
+	void DoDelete() final {};
+	void DoPost() final {};
+	void DoPut() final {};
 	};
 }
 #endif //UCENTRAL_RESTAPI_DEFAULT_CONFIGURATIONS_H
