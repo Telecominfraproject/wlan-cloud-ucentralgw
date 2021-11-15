515f6cdf912f8b5a23b341474f02e671df021652
stephb9959 <stephane.bourque@gmail.com>
2021-11-15 10:58:29 -0800

    New UUIDv4 Generator

--- src/framework/MicroService.h
+++ src/framework/MicroService.h
@@ -2968,8 +2968,36 @@ namespace OpenWifi {
 	}
 
 	[[nodiscard]] inline std::string MicroService::CreateUUID() {
-	    return UUIDGenerator_.create().toString();
-	}
+        static std::random_device              rd;
+        static std::mt19937                    gen(rd());
+        static std::uniform_int_distribution<> dis(0, 15);
+        static std::uniform_int_distribution<> dis2(8, 11);
+
+        std::stringstream ss;
+        int i;
+        ss << std::hex;
+        for (i = 0; i < 8; i++) {
+            ss << dis(gen);
+        }
+        ss << "-";
+        for (i = 0; i < 4; i++) {
+            ss << dis(gen);
+        }
+        ss << "-4";
+        for (i = 0; i < 3; i++) {
+            ss << dis(gen);
+        }
+        ss << "-";
+        ss << dis2(gen);
+        for (i = 0; i < 3; i++) {
+            ss << dis(gen);
+        }
+        ss << "-";
+        for (i = 0; i < 12; i++) {
+            ss << dis(gen);
+        };
+        return ss.str();
+    }
 
 	inline bool MicroService::SetSubsystemLogLevel(const std::string &SubSystem, const std::string &Level) {
 	    try {
