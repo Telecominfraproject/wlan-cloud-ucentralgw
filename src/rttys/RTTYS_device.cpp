68e4df6de38eb6f12a9397eec6ca3af15cd85ac2
stephb9959 <stephane.bourque@gmail.com>
2021-11-26 23:21:51 -0800

    Support both internal and external rttys

--- src/rttys/RTTYS_device.cpp
+++ src/rttys/RTTYS_device.cpp
@@ -169,9 +169,9 @@ namespace OpenWifi {
 				switch(msg) {
 					case msgTypeRegister: {
 						int pos=3;
-						id_ = std::dtring(&inBuf[3]);
-						desc_ = std::string(&inBuf[3 + id_.size() + 1]);
-						token_ = std::string(&nBuf[3 + id_.size() + 1 + desc_.size() + 1]);
+						id_ = std::string((char*)&inBuf[3]);
+						desc_ = std::string((char*)&inBuf[3 + id_.size() + 1]);
+						token_ = std::string((char*)&inBuf[3 + id_.size() + 1 + desc_.size() + 1]);
 
 						if(RTTYS_server()->ValidEndPoint(id_,token_)) {
 							RTTYS_server()->Logger().debug(Poco::format(
