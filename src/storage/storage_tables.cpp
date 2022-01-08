e3884ba06278cc11b272c0e4e972eb8a46ab64ae
stephb9959 <stephane.bourque@gmail.com>
2022-01-08 15:41:26 -0800

    Adding PING command and execution time for RPC.

--- src/storage/storage_tables.cpp
+++ src/storage/storage_tables.cpp
@@ -277,12 +277,12 @@ namespace OpenWifi {
 		try {
 			Poco::Data::Session Sess = Pool_->get();
 			if(dbType_==mysql) {
-				Sess << "alter table CommandList add column execution_time float" , Poco::Data::Keywords::now;
+				Sess << "alter table CommandList add column executionTime float" , Poco::Data::Keywords::now;
 			} else if (dbType_==pgsql || dbType_==sqlite) {
-				Sess << "alter table CommandList add column execution_time real" , Poco::Data::Keywords::now;
+				Sess << "alter table CommandList add column executionTime real" , Poco::Data::Keywords::now;
 			}
 		} catch (const Poco::Exception &E) {
-
+			Logger().log(E);
 		}
 		return -1;
 	}
