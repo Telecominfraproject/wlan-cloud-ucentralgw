c4a7c8dab221aaa36d2be940e12c7246d55dbdb2
stephb9959 <stephane.bourque@gmail.com>
2022-04-20 11:58:34 -0700

    Improving RTTYS

--- src/rttys/RTTYS_server.cpp
+++ src/rttys/RTTYS_server.cpp
@@ -95,7 +95,7 @@ namespace OpenWifi {
 		}
 
 		if(It->second.Device!= nullptr) {
-			std::cout << "login " << Id << std::endl;
+			std::cout << "login " << Id << " session " << It->second.Device->SessionID() << std::endl;
 			It->second.Device->Login();
 			std::cout << "login done" << Id << std::endl;
 			return true;
