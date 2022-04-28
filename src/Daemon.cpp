39d4423a5e86cce0b4e2c0c692a89a90de0ee2ac
stephb9959 <stephane.bourque@gmail.com>
2022-04-28 12:37:19 -0700

    Framework Update.

--- src/Daemon.cpp
+++ src/Daemon.cpp
@@ -36,6 +36,7 @@ namespace OpenWifi {
 										StorageService(),
 										SerialNumberCache(),
 										ConfigurationValidator(),
+								   		WebSocketClientServer(),
 										OUIServer(),
 										FindCountryFromIP(),
 										DeviceRegistry(),
