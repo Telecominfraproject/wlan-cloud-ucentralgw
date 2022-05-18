0750be2d8199a9e099df9d3a67bfe8a822475d05
stephb9959 <stephane.bourque@gmail.com>
2022-05-18 10:06:59 -0700

    Adding RadiusProxy processing.

--- src/RADIUS_proxy_server.cpp
+++ src/RADIUS_proxy_server.cpp
@@ -8,21 +8,29 @@ namespace OpenWifi {
 
 	int RADIUS_proxy_server::Start() {
 
+		std::cout << __LINE__ << std::endl;
 		Poco::Net::SocketAddress	AuthSockAddr(Poco::Net::AddressFamily::IPv4,
 									   MicroService::instance().ConfigGetInt("radius.proxy.authentication.port",21812));
+		std::cout << __LINE__ << std::endl;
 		AuthenticationSocket_.bind(AuthSockAddr,true);
+		std::cout << __LINE__ << std::endl;
 		Poco::Net::SocketAddress	AcctSockAddr(Poco::Net::AddressFamily::IPv4,
 									   MicroService::instance().ConfigGetInt("radius.proxy.accounting.port",21813));
+		std::cout << __LINE__ << std::endl;
 		AccountingSocket_.bind(AcctSockAddr,true);
 
+		std::cout << __LINE__ << std::endl;
 		AuthenticationReactor_.addEventHandler(AuthenticationSocket_,Poco::NObserver<RADIUS_proxy_server, Poco::Net::ReadableNotification>(
 														   *this, &RADIUS_proxy_server::OnAuthenticationSocketReadable));
 		AccountingReactor_.addEventHandler(AccountingSocket_,Poco::NObserver<RADIUS_proxy_server, Poco::Net::ReadableNotification>(
 																		  *this, &RADIUS_proxy_server::OnAccountingSocketReadable));
+		std::cout << __LINE__ << std::endl;
 
 		AuthenticationReactorThread_.start(AuthenticationReactor_);
+		std::cout << __LINE__ << std::endl;
 		AccountingReactorThread_.start(AccountingReactor_);
 
+		std::cout << __LINE__ << std::endl;
 		return 0;
 	}
 
