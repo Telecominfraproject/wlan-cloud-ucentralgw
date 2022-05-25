2ca1508be1de3c79c4af3d386c72c41980c26636
stephb9959 <stephane.bourque@gmail.com>
2022-05-25 09:45:28 -0700

    Adding ies request during WifiScan

--- src/ParseWifiScan.h
+++ src/ParseWifiScan.h
@@ -8,6 +8,18 @@
 
 namespace OpenWifi {
 
+	inline std::vector<unsigned char> Base64Decode(const std::string &F) {
+		std::istringstream ifs(F);
+		Poco::Base64Decoder b64in(ifs);
+		std::ostringstream ofs;
+		Poco::StreamCopier::copyStream(b64in, ofs);
+		std::vector<unsigned char> r;
+		for(const auto &c:ofs.str()) {
+			r.push_back((unsigned char)c);
+		}
+		return r;
+	}
+
 	inline bool ParseWifiScan(Poco::JSON::Object::Ptr &Obj, std::stringstream & Result) {
 		if(Obj->has("status")) {
 			auto Status = Obj->get("status").extract<Poco::JSON::Object::Ptr>();
@@ -21,7 +33,19 @@ namespace OpenWifi {
 						Poco::JSON::Array	new_ies;
 						for(auto &ie:*ies) {
 							auto ie_obj = ie.extract<Poco::JSON::Object::Ptr>();
-							std::cout << "IE:" << (uint64_t) ie_obj->get("type") << std::endl;
+							auto ie_type = (uint64_t) ie_obj->get("type");
+							auto ie_data = ie_obj->get("data").toString();
+							auto data = Base64Decode(ie_data);
+							if(ie_type==7) {
+								Poco::JSON::Object	new_ie;
+								std::string CountryName;
+								CountryName += data[0];
+								CountryName += data[1];
+								new_ie.set("country", CountryName);
+								new_ies.add(new_ie);
+							} else {
+								new_ies.add(ie_obj);
+							}
 						}
 						Entry->set("ies",new_ies);
 						ParsedScan.add(Entry);
