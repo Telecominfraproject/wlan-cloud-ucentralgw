712d326560b6f94bb8e6cd5a2c2169ee9e5c434d
stephb9959 <stephane.bourque@gmail.com>
2022-05-25 22:33:49 -0700

    Adding ies request during WifiScan

--- src/ParseWifiScan.h
+++ src/ParseWifiScan.h
@@ -271,7 +271,7 @@ namespace OpenWifi {
 			Inner.set("max_tx_power", (uint64_t)data[i + 2]);
 			constraints.add(Inner);
 		}
-		ie_data.set("country", CountryName);
+		// ie_data.set("country", CountryName);
 		// ie_data.set("constraints", constraints);
 		// new_ie.set("data", ie_data);
 		new_ie.set("name", "country");
