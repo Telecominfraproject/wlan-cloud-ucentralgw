af1e293bde017b100d501411f7a6d6c99ed2e545
stephb9959 <stephane.bourque@gmail.com>
2022-05-27 21:02:13 -0700

    Adding WifiScan Parsing WLAN_EID_SUPPORTED_REGULATORY_CLASSES
    Signed-off-by: stephb9959 <stephane.bourque@gmail.com>

--- src/ParseWifiScan.h
+++ src/ParseWifiScan.h
@@ -415,6 +415,13 @@ namespace OpenWifi {
 	inline nlohmann::json WFS_WLAN_EID_SUPPORTED_REGULATORY_CLASSES(const std::vector<unsigned char> &data) {
 		nlohmann::json 	new_ie;
 		nlohmann::json 	content;
+
+		std::cout << "Supported Regulatory Classes: " << data.size() << std::endl;
+		for(const auto &c:data) {
+			std::cout << (uint) c << " ";
+		}
+		std::cout << std::endl;
+
 		std::string Classes{data.begin(),data.end()};
 		std::cout << "Supported Regulatory Classes: " << data.size() << std::endl;
 		std::cout << "Classes: " << Classes << std::endl;
