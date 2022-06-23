62211b6e8bd1f5bad6a93c48b929054590e251e0
stephb9959 <stephane.bourque@gmail.com>
2022-06-22 21:36:20 -0700

    Adding deviceping command to CLI.
    Signed-off-by: stephb9959 <stephane.bourque@gmail.com>

--- src/RESTObjects/RESTAPI_GWobjects.h
+++ src/RESTObjects/RESTAPI_GWobjects.h
@@ -240,7 +240,7 @@ namespace OpenWifi::GWObjects {
 		RadiusProxyServerConfig	authConfig;
 		RadiusProxyServerConfig	acctConfig;
 		RadiusProxyServerConfig	coaConfig;
-		bool 		useByDefault=true;
+		bool 		useByDefault=false;
 
 		void to_json(Poco::JSON::Object &Obj) const;
 		bool from_json(const Poco::JSON::Object::Ptr &Obj);
