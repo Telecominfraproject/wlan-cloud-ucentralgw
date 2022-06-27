120f94a95a6edfb4a71f99934fc98d30f9866296
stephb9959 <stephane.bourque@gmail.com>
2022-06-27 13:51:16 -0700

    https://telecominfraproject.atlassian.net/browse/WIFI-9620
    Signed-off-by: stephb9959 <stephane.bourque@gmail.com>

--- src/RADIUS_helpers.h
+++ src/RADIUS_helpers.h
@@ -162,7 +162,7 @@ namespace OpenWifi::RADIUS {
 		std::string ExtractProxyStateDestination() {
 			std::string Result;
 			for(const auto &attribute:Attrs_) {
-				if(attribute.type==33) {
+				if(attribute.type==33 && attribute.len>0) {
 					std::string Attr33;
 					Attr33.assign((const char *)(const char *)&P_.attributes[attribute.pos],attribute.len);
 					auto Parts = Poco::StringTokenizer(Attr33,":");
@@ -175,8 +175,8 @@ namespace OpenWifi::RADIUS {
 		std::string ExtractCallingStationID() {
 			std::string Result;
 			for(const auto &attribute:Attrs_) {
-				if(attribute.type==31) {
-					Result.assign((const char *)(const char *)&P_.attributes[attribute.pos],attribute.len);
+				if(attribute.type==31 && attribute.len>0) {
+					Result.assign((const char *)(const char *)&P_.attributes[attribute.pos],attribute.len-1);
 					return Result;
 				}
 			}
@@ -186,8 +186,8 @@ namespace OpenWifi::RADIUS {
 		std::string ExtractCalledStationID() {
 			std::string Result;
 			for(const auto &attribute:Attrs_) {
-				if(attribute.type==30) {
-					Result.assign((const char *)(const char *)&P_.attributes[attribute.pos],attribute.len);
+				if(attribute.type==30 && attribute.len>0) {
+					Result.assign((const char *)(const char *)&P_.attributes[attribute.pos],attribute.len-1);
 					return Result;
 				}
 			}
