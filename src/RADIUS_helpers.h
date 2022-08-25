de20856cc19177928a27cb5be3359f6824c8e24d
stephb9959 <stephane.bourque@gmail.com>
2022-08-25 09:29:06 -0700

    https://telecominfraproject.atlassian.net/browse/WIFI-10340
    Signed-off-by: stephb9959 <stephane.bourque@gmail.com>

--- src/RADIUS_helpers.h
+++ src/RADIUS_helpers.h
@@ -252,7 +252,7 @@ static const struct tok radius_command_values[] = {
 			BufLog(os, "", P_.authenticator, sizeof(P_.authenticator));
 			os << "  Attributes: " << std::endl;
 			for(const auto &attr:Attrs_) {
-				os << "    " << (uint) attr.type << "   Len:" << attr.len << "   " ;
+				os << "    " << (uint) attr.type << "   Len:" << attr.len << std::endl;
 				BufLog(os, "        " , &P_.attributes[attr.pos], attr.len);
 			}
 			os << std::dec << std::endl << std::endl;
