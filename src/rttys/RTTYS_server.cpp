86a33ed81896ffe6165271b0ced44bb83467cbc9
stephb9959 <stephane.bourque@gmail.com>
2023-03-23 11:21:52 -0700

    https://telecominfraproject.atlassian.net/browse/WIFI-12337
    Signed-off-by: stephb9959 <stephane.bourque@gmail.com>

--- src/rttys/RTTYS_server.cpp
+++ src/rttys/RTTYS_server.cpp
@@ -460,7 +460,8 @@ namespace OpenWifi {
 					continue;
 				}
 
-				std::cout << "BufferCurrentSize: " << BufferCurrentSize << "  MsgLen: " << msg_len << " Pos: " << BufferPos << std::endl;
+				std::cout << "Cmd: " << (int) LastCommand << "  BufferCurrentSize: " << BufferCurrentSize
+						  << "  MsgLen: " << msg_len << "  Pos: " << BufferPos << std::endl;
 				switch (LastCommand) {
 					case RTTYS_EndPoint::msgTypeRegister: {
 						good = do_msgTypeRegister(pNf->socket(), Buffer, BufferCurrentSize, BufferPos, msg_len);
