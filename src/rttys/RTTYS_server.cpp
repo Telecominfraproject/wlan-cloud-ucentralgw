f143471723f093e3fc5fc3df7eaf92639c7a7f2c
stephb9959 <stephane.bourque@gmail.com>
2023-03-23 21:31:29 -0700

    https://telecominfraproject.atlassian.net/browse/WIFI-12337
    Signed-off-by: stephb9959 <stephane.bourque@gmail.com>

--- src/rttys/RTTYS_server.cpp
+++ src/rttys/RTTYS_server.cpp
@@ -1067,7 +1067,6 @@ namespace OpenWifi {
 
 	bool RTTYS_server::do_msgTypeHeartbeat(const Poco::Net::Socket &Socket, [[maybe_unused]] Poco::FIFOBuffer &buffer, [[maybe_unused]] std::size_t msg_len) {
 		try {
-			std::cout << "Sending heartbeat" << std::endl;
 			u_char MsgBuf[RTTY_HDR_SIZE + 16]{0};
 			MsgBuf[0] = RTTYS_EndPoint::msgTypeHeartbeat;
 			MsgBuf[1] = 0;
