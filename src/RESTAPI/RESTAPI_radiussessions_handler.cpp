4f289427ca9a3f8243fc13a6f4c450d6827e63cf
stephb9959 <stephane.bourque@gmail.com>
2023-04-03 12:11:42 -0700

    https://telecominfraproject.atlassian.net/browse/WIFI-12423
    Signed-off-by: stephb9959 <stephane.bourque@gmail.com>

--- src/RESTAPI/RESTAPI_radiussessions_handler.cpp
+++ src/RESTAPI/RESTAPI_radiussessions_handler.cpp
@@ -17,7 +17,7 @@ namespace OpenWifi {
 		}
 
 		auto SerialNumber = GetBinding("serialNumber","");
-		if(SerialNumber.empty() || Utils::ValidSerialNumber(SerialNumber)) {
+		if(SerialNumber.empty() || !Utils::ValidSerialNumber(SerialNumber)) {
 			return BadRequest(RESTAPI::Errors::MissingOrInvalidParameters);
 		}
 
