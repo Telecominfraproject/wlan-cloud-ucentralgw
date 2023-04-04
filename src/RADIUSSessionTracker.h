7ebc97760124282b10afd0aef8e6393fd9e933ca
stephb9959 <stephane.bourque@gmail.com>
2023-04-04 09:18:12 -0700

    https://telecominfraproject.atlassian.net/browse/WIFI-12423
    Signed-off-by: stephb9959 <stephane.bourque@gmail.com>

--- src/RADIUSSessionTracker.h
+++ src/RADIUSSessionTracker.h
@@ -91,7 +91,7 @@ namespace OpenWifi {
 
 			for(const auto &[_,sessions]:AccountingSessions_) {
 				for(const auto &[_,session]:sessions) {
-					if(session->userName==userName) {
+					if(Utils::match(session->userName.c_str(),userName.c_str())) {
 						list.sessions.emplace_back(*session);
 					}
 				}
@@ -103,7 +103,7 @@ namespace OpenWifi {
 
 			for(const auto &[_,sessions]:AccountingSessions_) {
 				for(const auto &[_,session]:sessions) {
-					if(session->callingStationId==mac) {
+					if(Utils::match(session->callingStationId.c_str(),mac.c_str())) {
 						list.sessions.emplace_back(*session);
 					}
 				}
--- src/framework/utils.h
+++ src/framework/utils.h
@@ -146,4 +146,38 @@ namespace OpenWifi::Utils {
 
 	bool ExtractBase64CompressedData(const std::string &CompressedData,
 									 std::string &UnCompressedData, uint64_t compress_sz);
+
+	inline bool match(const char* first, const char* second)
+	{
+		// If we reach at the end of both strings, we are done
+		if (*first == '\0' && *second == '\0')
+			return true;
+
+		// Make sure to eliminate consecutive '*'
+		if (*first == '*') {
+			while (*(first + 1) == '*')
+				first++;
+		}
+
+		// Make sure that the characters after '*' are present
+		// in second string. This function assumes that the
+		// first string will not contain two consecutive '*'
+		if (*first == '*' && *(first + 1) != '\0'
+			&& *second == '\0')
+			return false;
+
+		// If the first string contains '?', or current
+		// characters of both strings match
+		if (*first == '?' || *first == *second)
+			return match(first + 1, second + 1);
+
+		// If there is *, then there are two possibilities
+		// a) We consider current character of second string
+		// b) We ignore current character of second string.
+		if (*first == '*')
+			return match(first + 1, second)
+				   || match(first, second + 1);
+		return false;
+	}
+
 } // namespace OpenWifi::Utils
