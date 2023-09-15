960640cc0512d6c00fd4f4e7cff5bdf21be92e3d
stephb9959 <stephane.bourque@gmail.com>
2023-09-15 10:08:18 -0700

    https://telecominfraproject.atlassian.net/browse/WIFI-7831
    Signed-off-by: stephb9959 <stephane.bourque@gmail.com>

--- src/RADSEC_server.h
+++ src/RADSEC_server.h
@@ -62,10 +62,10 @@ namespace OpenWifi {
 					P.MakeStatusMessage();
 
 					poco_information(Logger_, "Keep-Alive message.");
-					std::cout << Server_.radsecSecret << std::endl;
-					RADIUS::RadiusPacket	PP(P.Data(),P.Len());
-					PP.Log(std::cout);
-//					Socket_->sendBytes(P.Data(), P.Len());
+//					std::cout << Server_.radsecSecret << std::endl;
+//					RADIUS::RadiusPacket	PP(P.Data(),P.Len());
+//					PP.Log(std::cout);
+					Socket_->sendBytes(P.Data(), P.Len());
 					LastStatus = Utils::Now();
 				}
 				Poco::Thread::trySleep(!Connected_ ? 30000 : 2000);
