132b31b06b469cb759af2fa99fb262a69c1ceb0c
Ivan Chvets <ivan.chvets@kinarasystems.com>
2024-09-11 15:50:30 -0400

    feat: add fixedconfig command https://telecominfraproject.atlassian.net/browse/WIFI-13126
    Summary of changes:
    - Added `fixedconfig` command.
    
    Signed-off-by: Ivan Chvets <ivan.chvets@kinarasystems.com>

--- src/RESTObjects/RESTAPI_GWobjects.h
+++ src/RESTObjects/RESTAPI_GWobjects.h
@@ -534,4 +534,10 @@ namespace OpenWifi::GWObjects {
 
 		bool from_json(const Poco::JSON::Object::Ptr &Obj);
 	};
+	struct FixedConfig {
+		std::string 	serialNumber;
+		std::string 	country;
+
+		bool from_json(const Poco::JSON::Object::Ptr &Obj);
+	};
 } // namespace OpenWifi::GWObjects
